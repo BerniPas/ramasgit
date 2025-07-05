1f1fe3d388a6f53729230918439ce5918f72cc7d
BerniPas <bernalpas@gmail.com>
2025-07-05 12:11:44 -0300

    Update Navegacion.jsx

--- practica02-react/src/Componenetes/Navegacion.jsx
+++ practica02-react/src/Componenetes/Navegacion.jsx
@@ -12,6 +12,7 @@ function Navegacion() {
                 <Navbar.Collapse id="basic-navbar-nav">
                     <Nav className="me-auto">
                         <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#home">Colaboración</Nav.Link>
                         <Nav.Link href="#link">Link</Nav.Link>
                         <NavDropdown title="Dropdown" id="basic-nav-dropdown">
                             <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
